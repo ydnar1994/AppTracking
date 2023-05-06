8972692521b1ee132b774058146f0d5c83664416
rekiyosep <reki.pebriana@REKI-YOSEP.multipolar.com>
2023-05-06 12:18:03 +0700

    Test Commit App Tracking

--- src/apptracking/main/AppTracking.java
+++ src/apptracking/main/AppTracking.java
@@ -17,6 +17,8 @@ public class AppTracking {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Test Push");
     }
     
 }
